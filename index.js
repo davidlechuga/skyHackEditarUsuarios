5087cd436e9e63c56de4932b62fa3c7e7f3c2d46
davidlechuga <47370612+davidlechuga@users.noreply.github.com>
2020-05-13 15:08:13 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+import gql from 'graphql-tag';
+
+export const NUEVO_USUARIO = gql`
+	mutation crearUsuario($input: UsuarioInput) {
+		crearUsuario(input: $input) {
+			id
+			nombre
+			apellido
+		}
+	}
+`;
+
+export const MODIFICAR_USUARIO = gql`
+	mutation modificarUsuario($input: UsuarioInput) {
+		modificarUsuario(input: $input) {
+			id
+			nombre
+			apellido
+			empresa
+			emails {
+				email
+			}
+			edad
+			tipo
+		}
+	}
+`;
